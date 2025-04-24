a3b9153ea6a3764a891e058cab6436e5c7909ba8
ArthurOReis <arthuroreis@2005.com>
2025-04-24 08:31:35 -0300

    Instalando e testando biblioteca Draw

---
+++ app/src/main/java/ifsc/poo/Gradle.java
@@ -0,0 +1,15 @@
+package ifsc.poo;
+
+public class Gradle {
+    private int linhas;
+    private int colunas;
+
+    public Gradle(int linhas, int colunas){
+        this.linhas = linhas;
+        this.colunas = colunas;
+    }
+
+    // public Draw desenhar(Draw obj){
+
+    // }
+}
